a372f3084c90d85cc9a52524809b69c2fdd4eb69
Sebastian Peralta <peraltasjp@gmail.com>
2021-05-07 15:52:00 -0500

    hello

---
+++ src/hello.cpp
@@ -0,0 +1,12 @@
+/* hello.cpp */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Please enter your name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name;
+    cout << "\n";
+    return 0;
+}
