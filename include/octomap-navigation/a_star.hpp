86540f2d6bdd0b0ff8a68c39c00006fa31b23eab
Damian Owerko <owerko@seas.upenn.edu>
2021-05-09 17:10:59 -0400

    Remove cost function from graph

--- include/octomap-navigation/a_star.hpp
+++ include/octomap-navigation/a_star.hpp
@@ -1,26 +1,28 @@
+#pragma once
 #include <vector>
 #include <tuple>
 #include <array>
 #include <queue>
-#include <octomap/octomap.h>
-#include <octomap/OcTree.h>
 
-template<class T>
-class Graph {
+template <class T>
+class Graph
+{
 public:
     virtual std::vector<T> neighbors(T node);
-    virtual float cost(T node1, T node2);
 };
 
 typedef std::array<size_t, 3> coordinate;
 
-class GridGraph : Graph<coordinate> {
+class GridGraph : Graph<coordinate>
+{
 public:
     GridGraph(coordinate size, std::vector<bool> data) : size(size), data(data) {}
 
-    std::vector<coordinate> neighbors(coordinate node){
-        // TODO: Implement        
+    std::vector<coordinate> neighbors(coordinate node)
+    {
+        // TODO: Implement
     }
+
 private:
     coordinate size;
     std::vector<bool> data;
@@ -29,7 +31,7 @@ private:
 /**
  * @brief 
  */
-template<class T>
-float a_star (Graph<T> graph, T start, T end, float (*function)(T,T) heuristic) {
+template <class T>
+float a_star(Graph<T> graph, T start, T end, float (*function)(T, T) heuristic){
     // TODO: implement
 };
