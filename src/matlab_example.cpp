fccd4f099c8890f1c7b53f37e82730dabcdc0390
Damian Owerko <owerko@seas.upenn.edu>
2021-05-10 23:02:12 -0400

    matlab plto

--- src/matlab_example.cpp
+++ src/matlab_example.cpp
@@ -1,8 +1,16 @@
 #include "MatlabEngine.hpp"
 #include "MatlabDataArray.hpp"
+#include <iostream>
 
 using namespace matlab::engine;
 
-int main() {
+int main()
+{
     std::unique_ptr<MATLABEngine> matlabPtr = startMATLAB();
+    matlab::data::ArrayFactory factory;
+    std::vector<matlab::data::Array> args({
+        factory.createArray<double>({1, 10}, {4, 8, 6, -1, -2, -3, -1, 3, 4, 5})
+    });
+    matlabPtr->feval(u"plot", args);
+    std::cin.get();
 }
